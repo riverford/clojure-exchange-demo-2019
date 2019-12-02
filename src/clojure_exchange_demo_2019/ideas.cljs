97108536094e1aa760248aff1ce5b6f6327cacd3
Daniel Neal <danielneal@riverford.co.uk>
2019-12-02 11:38:27 +0200

    Remove other people's ideas!

--- src/clojure_exchange_demo_2019/ideas.cljs
+++ src/clojure_exchange_demo_2019/ideas.cljs
@@ -1,16 +1,12 @@
 ;; 1. buoyancy - app for helping ideas rise
-;; 2. jester - chat app where you store the gestures x
-;; 3. constellation - dot to dot icebreaker
-;; 4. food chain - meal planner and fridge management system x
-;; 5. tuber - ride sharing app x
-;; 6. dream diary - with 16 dream questions
-;; 7. power ranges - track your strength in different rep ranges
-;; 8. resonance - talking back app for focusing x
-;; 9. multiway - a multiway dictionary for meaning
-;; 10. betting app for positivity x
-;; 11. focusing map to the body
-;; 12. allchestra
-;; 13. amino acid profile tracker for vegans
-;; 14. language learning app
-;; 15. ferment tracker - track
-;; 16. supermarket sweep - put in the aisles in your supermarket and your shopping list and get the pick order
+;; 2. constellation - dot to dot icebreaker
+;; 3. food chain - meal planner and fridge management system x
+;; 4. dream diary - with 16 dream questions
+;; 5. power ranges - track your strength in different rep ranges
+;; 6. resonance - talking back app for focusing x
+;; 7. arrows - a multiway dictionary for meaning
+;; 8. focusing map to the body
+;; 9. amino acid profile tracker for vegans
+;; 10. language learning app
+;; 11. ferment tracker - track
+;; 12. supermarket sweep - put in the aisles in your supermarket and your shopping list and get the pick order
